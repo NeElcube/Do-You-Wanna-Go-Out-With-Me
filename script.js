2cdbfed39a11dd7d4364e96b1884cb26625215ed
NeElcube <eliaslallinger2@gmail.com>
2024-07-27 00:38:58 +0200

    addsound

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let moveContainer = document.getElementById('move');
+    let moveContainer = document.getElementById('move')
 
     document.addEventListener('mousemove', function (e) {
         let x = e.clientX;
         let y = e.clientY;
-        moveContainer.style.left = x + "px";
-        moveContainer.style.top = y + "px";
-    });
-});
+        moveContainer.style.left = x + "px"
+        moveContainer.style.top = y + "px"
+    })
+})
+
+function yes() {
+    var audio = document.getElementById("yay")
+    audio.play();
+    audio.onended = function() {
+        window.location.href = 'yes.html'
+    }
+}
